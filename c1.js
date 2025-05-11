406283364a897a89aee5d2f20e1b3532ec44b125
priyanshu-cd <priyanshusrma01@gmail.com>
2025-05-11 04:28:22 -0700

    comments in js

--- c1.js
+++ c1.js
@@ -1,8 +1,3 @@
-let person_name = "Priyanshu"
-
-const const_person = "Monika"
-console.log(person_name)
-console.log("Const", const_person)
-
-person_name = const_person
-console.log(person_name)
+let num
+// num = 7
+console.log(num) // Undefined
