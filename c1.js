0e08d95c568e14780a341529dfc64081f0c20fa0
priyanshu-cd <priyanshusrma01@gmail.com>
2025-05-11 04:52:45 -0700

    Data Types, Prevent'

--- c1.js
+++ c1.js
@@ -1,3 +1,23 @@
-let num
-// num = 7
-console.log(num) // Undefined
+// Data types
+// Number , String , null , undefined , Boolean(true,false)
+
+//object (like dict) :
+let self = "Priyanshu"
+
+let family = {
+//key(variable): value,
+    father:"Rakesh",
+    mother:"Monika",
+    sister:"Prerna",
+    me:self,
+    nest:{nest2:self},
+}
+console.log("\'Object\'",family);
+console.log(family.me);
+
+
+//Array (like list) :
+let arr = [1,2,3,4,5,6,99]
+console.log("\'Array\'",arr);
+console.log("Last element in array is",arr[arr.length - 1]);
+
