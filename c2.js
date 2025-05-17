e114a435c5b248dc0473d6622c01581526b5585f
priyanshu-cd <priyanshusrma01@gmail.com>
2025-05-17 04:08:35 -0700

    List Operations

--- c2.js
+++ c2.js
@@ -1,38 +1,58 @@
-str = "Priyanshu Sharma"
-// 0,1,2,3,4
-//p,r,i,y,a
-//index 0 -> .length -1
-console.log(str[4]) //a
+//List oprations:
+const str = "Priyanshu is great dev"
 
-// string concanation (string add)
-let combine = str + " is great"
-console.log(combine) //Priyanshu Sharma is great
+let arr = str.split(' ')
+console.log(arr); //[ 'Priyanshu', 'is', 'great', 'dev' ]
 
-//Integer add string
-console.log('6' + 4) // in '+' =  64
-console.log('6' - 4) // in '-' = 2 // type values changes..
+//Read
+console.log(arr[0]) //Priyanshu (index = 0)
 
-//Function of strings:
-const index_of_p = str.indexOf('P') //Case sensative..
-console.log(index_of_p);  // 0
+//change
+arr[0] = "Tarun"
+console.log(arr); //[ 'Tarun', 'is', 'great', 'dev' ]
 
-console.log(str.includes('P')) // true , check and return bool if exist true.
+//Add
+arr.push("Lie")
+console.log(arr); //[ 'Tarun', 'is', 'great', 'dev', 'Lie' ]
 
-/*Split*/
-console.log(str.split()) // [ 'Priyanshu Sharma' ] , all into one string list
-console.log(str.split('')) /*
-  'P', 'r', 'i', 'y',
-  'a', 'n', 's', 'h',
-  'u', ' ', 'S', 'h',
-  'a', 'r', 'm', 'a'
-] , each character into list */
-console.log(str.split(' '));  // each word into list..
+//POP()
+arr.pop() //remove last
+console.log(arr); //[ 'Tarun', 'is', 'great', 'dev' ]
 
-                        // which , what
-let str2 = str + " is great dev"
-str2 = str2.replaceAll(' ','_') // also replace() , change first found only...
-console.log(str2); // Priyanshu_Sharma_is_great_dev
+//Operations: all like string , includes(value)=bool ,indexOf("dev") .slice(start_index)etc
+console.log(arr.join()) //Tarun,is,great,dev
+console.log(arr.join(" ")) //Tarun is great dev (join with " ", "_")
 
-                    // start , stop
-console.log(str2.slice(8,14)) //u_Shar
+console.log(arr.sort()) //[ 'Tarun', 'dev', 'great', 'is' ] alphbetically sorted..
 
+//MULTIDIMENTION ARRAY:
+arr2 = [[3] , [4] , [5]] 
+arr2[0][0] = 99
+console.log(arr2[0][0]) // 99
+
+for(let i = 0;i<arr2.length;i++){
+    list1=arr2[i]
+    
+    for(let j=0;j<list1.length;j++){
+        console.log(list1[j] , arr2[i][j]);
+        // console.log(arr2[i][j]);   
+    }
+}
+/*
+99 99
+4 4
+5 5 */
+
+//Some method changes the whole array then assign:
+/*
+arr4 = arr3.pop() , sort() = 
+                    arr3.pop()
+                    arr4=arr3
+*/
+
+// Add 2 LISTS...
+//      0       1       2       3          
+// [ 'Tarun', 'is', 'great', 'dev' ]
+list1=arr.slice(0,2) // [ 'Tarun', 'is' ] ,2 is exclude
+list2 = arr.slice(3) // [ 'dev' ] 
+console.log(list1.concat(list2));
